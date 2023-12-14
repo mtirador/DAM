102750421559677ff9ad121219f107ddabcbac85
Marta Tirador <martatiradorgutierrez@gmail.com>
2023-12-14 09:16:48 +0100

    add files

--- Acceso de Datos/Tema2_Consultas/Bloque2_Extra/Consultas2_Extra.sql
+++ Acceso de Datos/Tema2_Consultas/Bloque2_Extra/Consultas2_Extra.sql
@@ -2,24 +2,47 @@
 
 --PROVINCIAS:
 
--- (32) - Provincia más poblada de las inferiores a 1 millón de habitantes
+-- Provincia más poblada de las inferiores a 1 millón de habitantes
 
--- (35) - ¿En qué autonomía está la provincia más extensa?
+select provincia from provincias where poblacion < 1000000 order by poblacion desc limit 1;
 
--- (36) - ¿Qué provincias tienen una población por encima de la media nacional?
 
--- (44) - ¿Qué porcentaje del total nacional representa Cantabria en población?
+--Ģmax(poblacion)(σprovincia<1000000)(provincias) -->c1
+--Πprovincia(σprovincias)=c1;
 
--- (47) - ¿En qué posición del ranking autonómico por población de mayor a menor está Cantabria?
+select distinct provincia from provincias where poblacion = (select max(poblacion) from provincias where poblacion<1000000);
+
+
+-- ¿En qué autonomía está la provincia más extensa?
+
+--selecciono el maximo de las superficies de la tabla provincias. (1registro)
+--Ģmax(poblacion)(provincias)
+ select max(superficie)from provincias; -->c1
+ 
+--Πprovincia(σsuperficie=(c1))
+ select distinct autonomia from provincias where superficie = (select max(superficie)from provincias);
+ 
+
+-- ¿Qué provincias tienen una población por encima de la media nacional?
+
+--cual es la media de poblacion nacional
+--ĢAVG(poblacion)(provincias)-->c1
+select avg(poblacion)from provincias;
+--Πprovincia(σpoblacion>(c1))
+select distinct provincia from provincias where poblacion>(select avg(poblacion)from provincias);
+
+-- ¿Qué porcentaje del total nacional representa Cantabria en población?
+
+-- ¿En qué posición del ranking autonómico por población de mayor a menor está Cantabria?
 
 --CICLISTAS: 
 
--- (l) - Obtener el nombre de los puertos de monta~na que tienen una altura superior a la altura media de todos los puertos
+-- Obtener el nombre de los puertos de montaña que tienen una altura superior a la altura media de todos los puertos
 
--- 8(g) Obtener el nombre y el equipo de los ciclistas que han llevado alg un maillot o que han ganado algun puerto. Muestra la lista ordenada. Recuerda la diferencia entre UNION y UNION ALL
+-- Obtener el nombre y el equipo de los ciclistas que han llevado alg un maillot o que han ganado algun puerto. Muestra la lista ordenada. Recuerda la diferencia entre UNION y UNION ALL
 
--- 9(07b) - ¿Qué c odigos de maillots ha llevado Alfonso Guti errez en los puertos que haya ganado
+-- ¿Qué codigos de maillots ha llevado Alfonso Guti errez en los puertos que haya ganado
 
--- 10(s) - ¿Qué equipos no han ganado ningún puerto de montaña?
+-- ¿Qué equipos no han ganado ningún puerto de montaña?
 
--- 8(p) Obtener el nombre de los ciclistas, ordenados alfabéticamente, que pertenecen a un equipo de más de cinco ciclistas y que han ganado alguna etapa, indicando también cuántas etapas han ganado
+-- Obtener el nombre de los ciclistas, ordenados alfabéticamente, que pertenecen a un equipo de más de cinco ciclistas y que han ganado alguna etapa, indicando también cuántas etapas han ganado
